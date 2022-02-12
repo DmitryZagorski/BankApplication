1d768a1a090c58d6538b36ad16b1a82a5f002d10
DmitryZagorski <d375298856600@ya.ru>
2022-02-12 18:43:17 +0300

    Realisation if methods in greeting

--- src/main/java/home/intexsoft/bank_application/Main.java
+++ src/main/java/home/intexsoft/bank_application/Main.java
@@ -3,9 +3,8 @@ package home.intexsoft.bank_application;
 public class Main {
 
     public static void main(String[] args) {
-        UserInputParser userInputParser = new UserInputParser();
-        userInputParser.printCommands();
-        CommandDescriptor commandDescriptor = userInputParser.parse(userInputParser.enterCommand());
+
+
 
 
     }
--- src/main/java/home/intexsoft/bank_application/greeting/Greeting.java
+++ src/main/java/home/intexsoft/bank_application/greeting/Greeting.java
@@ -1,26 +1,38 @@
 package home.intexsoft.bank_application.greeting;
 
+import home.intexsoft.bank_application.commands.BanksCommands;
+import home.intexsoft.bank_application.commands.ClientsCommands;
+import home.intexsoft.bank_application.commands.Commands;
+import home.intexsoft.bank_application.commands.TransactionsCommands;
+
+import java.util.Arrays;
+
 public class Greeting {
 
     public void firstPhrase() {
         System.out.println("Hello. Let's start our application.");
     }
 
+    public void chooseCommand() {
+        System.out.println("Enter one of these commands");
+    }
+
     public void printFirstCommands() {
-
+        System.out.println(Arrays.toString(Commands.values()));
     }
 
-    public void printBankCommands(){
-
+    public void printBankCommands() {
+        System.out.println(Arrays.toString(BanksCommands.values()));
     }
 
-    public void printClientCommands(){
-
+    public void printClientCommands() {
+        System.out.println(Arrays.toString(ClientsCommands.values()));
     }
 
-    public void printTransactionCommand(){
-
+    public void printTransactionCommand() {
+        System.out.println(Arrays.toString(TransactionsCommands.values()));
     }
+
 
 
 }
